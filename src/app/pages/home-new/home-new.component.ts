7160537163045106d59741fe5214ac821b564b02
Baudouin Keunne <keunne.baudoin@yahoo.fr>
2023-02-21 22:00:51 +0100

    add button to stop scann

--- src/app/pages/home-new/home-new.component.ts
+++ src/app/pages/home-new/home-new.component.ts
@@ -34,10 +34,8 @@ export class HomeNewComponent implements OnInit, OnDestroy {
       const status = await BarcodeScanner.checkPermission({ force: true });
       if (status.granted) {
         // the user granted permission
-        this.permission = true;
         return true;
       }
-      this.permission = false;
       return false;
     } catch(e) {
       console.log(e);
@@ -53,10 +51,12 @@ export class HomeNewComponent implements OnInit, OnDestroy {
       await BarcodeScanner.hideBackground();
       document.querySelector('body')?.classList.add('scanner-active');
       this.content_visibility = 'hidden';
+      this.scanActive = true;
       const result = await BarcodeScanner.startScan();
       console.log(result);
       BarcodeScanner.showBackground();
       document.querySelector('body')?.classList.remove('scanner-active');
+      this.scanActive = false;
       this.content_visibility = '';
       if(result?.hasContent) {
         this.scannedResult = result.content;
@@ -73,6 +73,7 @@ export class HomeNewComponent implements OnInit, OnDestroy {
     BarcodeScanner.stopScan();
     document.querySelector('body')?.classList.remove('scanner-active');
     this.content_visibility = '';
+    this.scanActive = false;
   }
 
   ngOnDestroy(): void {
