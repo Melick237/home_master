9ed0490761e26ae5bbe0a1032d7c53748ebb854d
Baudouin Keunne <keunne.baudoin@yahoo.fr>
2023-02-24 00:18:14 +0100

    finish tasks screen

--- src/app/pages/dashboard/tasks/tasks.component.ts
+++ src/app/pages/dashboard/tasks/tasks.component.ts
@@ -10,7 +10,7 @@ export class TasksComponent implements OnInit {
   public tasks = [
     {
       name: "Kitchen Cleaning",
-      dueDate: new Date("25/02/2023"),
+      dueDate: new Date("2023/02/21"),
       room: 1,
       assignedTo: {
         firstName: "Leonardo",
@@ -30,7 +30,7 @@ export class TasksComponent implements OnInit {
     },
     {
       name: "Bathroom Cleaning",
-      dueDate: new Date("28/02/2023"),
+      dueDate: new Date("2023/02/26"),
       room: 1,
       assignedTo: {
         firstName: "Leonardo",
@@ -43,5 +43,17 @@ export class TasksComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {}
+
+  getDays(date1: Date) : number {
+        // To calculate the time difference of two dates
+        var Difference_In_Time = date1.getTime() - new Date().getTime();
+
+        // To calculate the no. of days between two dates
+        var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+
+        console.log(Difference_In_Days)
+
+    return Math.round(Difference_In_Days);
+  }
 
 }
